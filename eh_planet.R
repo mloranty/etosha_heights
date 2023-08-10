823459b166512daa54c8beeb1a407bc592989ce2
Mike Loranty <mloranty@colgate.edu>
2023-08-10 10:56:49 -0400

    updated code and added a project notes file

--- eh_planet.R
+++ eh_planet.R
@@ -14,3 +14,11 @@ library(tidyverse)
 
 #set working directory
 setwd("G:/My Drive/Documents/research/giraffe")
+
+
+mos <- list.files(pattern = "composite.tif", recursive = T, full.names = T)
+
+t <- rast(mos[1])
+
+u <- rast(mos[2])
+compa
