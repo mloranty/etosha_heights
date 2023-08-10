6484f0ef3f06b97a95b2bc5d2d233136cf525d5a
Mike Loranty <mloranty@colgate.edu>
2023-08-10 09:46:34 -0400

    Added file to analyze planet timerseries

---
+++ eh_plant.R
@@ -0,0 +1,16 @@
+######################################
+#
+# analyze phenology for Etosha Heights 
+# using Planet Imagery
+#
+# MML 08/10/2023
+######################################
+
+rm(list = ls())
+
+#load required packages
+library(terra)
+library(tidyverse)
+
+#set working directory
+setwd("G:/My Drive/Documents/research/giraffe")
