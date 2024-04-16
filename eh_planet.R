ffe4d0e95f5e31e9b85cf2c9f776a8f217d33d16
Mike Loranty <mloranty@colgate.edu>
2024-04-16 08:59:53 -0400

    added figures to look at VIs across different veg types

--- eh_planet.R
+++ eh_planet.R
@@ -67,25 +67,90 @@ eo <- paste("eh_planet/evi/",f,"_evi.tif", sep = "")
 # evi output filenames
 so <- paste("eh_planet/savi/",f,"_savi.tif", sep = "")
 
+#reference raster to align everything to the same extent
+ref <- rast(mos[1])
 
 # calculate evi and savi and write to file
-
+# note we ran into some memory issues here
 for(i in 1:length(mos))
 {
   x <- rast(mos[i])
   e <- evi(x)
   s <- savi(x)
-  writeRaster(e,eo[i], overwrite = T)
-  writeRaster(s,so[i], overwrite = T)
+  e <- resample(e,ref, filename = eo[i], overwrite = T)
+  s <- resample(s,ref, filename = so[i], overwrite = T)
+ # writeRaster(e,eo[i], overwrite = T)
+ # writeRaster(s,so[i], overwrite = T)
   rm(x,s,e)
 }
 
 
+# read eh veg data for validation
+vg <-read.csv("eh_veg_data/DB_EtoshaHeights_VegTransects.csv", 
+              header = T)
+
+# convert to spatial vector
+veg <- vect(vg, geom = c("lon_dd", "lat_dd"), crs = "epsg:4326")
+
+#veg.utm <- project(veg, ref)
+# read in the stacks of evi and savi data
+eh.evi <- rast(list.files(path = "eh_planet/evi/", full.names = T))
+eh.savi <- rast(list.files(path = "eh_planet/savi/", full.names = T))
+
+# extract evi and savi values for each plot
+plt.evi <- terra::extract(eh.evi,veg)
+plt.savi <- terra::extract(eh.savi,veg)
+
+# set column headers based on file names
+colnames(plt.evi) <- c("ID", substr(list.files(path = "eh_planet/evi/"),1,11))
+colnames(plt.savi) <- c("ID", substr(list.files(path = "eh_planet/savi/"),1,11))
+
+# add variables for analysis/plotting
+plt.evi <- cbind(plt.evi, veg[,1:6])
+plt.savi <- cbind(plt.savi, veg[,1:6])
+
+# pivot longer 
+pel <- pivot_longer(plt.evi, cols = starts_with("EH"))
+psl <- pivot_longer(plt.savi, cols = starts_with("EH"))
+
+# change varibale names
+pel <- rename(pel, evi = value)
+psl <- rename(psl, savi = value)
+
+# join these dataframes
+plt.vi <- full_join(pel, psl)
+rm(pel,psl,plt.evi,plt.savi)
+
+# add time stamp here
+plt.vi$timestamp <- strptime(as.numeric(substr(plt.vi$name,4,11)), 
+                            format = "%Y%m%d", usetz = FALSE)
 
 
+# group by veg class
+vcl <- plt.vi %>%
+  group_by(Association,timestamp) %>%
+  summarise(evi = mean(evi, na.rm = T),
+            svi = mean(savi, na.rm = T),
+            elev_m = mean(elev_m, na.rm = T))
+
+vcl$ts <- as.POSIXct(vcl$timestamp)
+
+e <- ggplot() +
+  geom_point(data = vcl, aes(x = ts, y = evi, color = Association, group = Association))
+
+s <- ggplot() +
+  geom_point(data = vcl, aes(x = ts, y = svi, color = Association, group = Association))
 
 wd(ts)
 wy(ts)
+
+
+
+e <- list.files(path = "eh_planet/evi/", full.names = T)
+s <- list.files(path = "eh_planet/savi/", full.names = T)
+e1 <- rast(e[1])
+e2 <- rast(e[2])
+e3 <- rast(e[3])
 
 t <- rast(mos[1])
 et <- evi(t)
