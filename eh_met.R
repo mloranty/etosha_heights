7fa220f500c8454fed97e78f96ac1324ff5c0310
Mike Loranty <mloranty@colgate.edu>
2024-09-10 13:37:33 -0400

    updating met code

--- eh_met.R
+++ eh_met.R
@@ -3,21 +3,17 @@
 # analyze Etosha Heights micromet data
 #
 # MML 09/09/2024
-<<<<<<< HEAD
 ######################################
 
 rm(list = ls())
 
-#load required packages
-library(terra)
-library(tidyverse)
-library(patchwork)
-library(tidyterra)
-library(caret)
-library(randomForest)
 #set working directory
-#setwd("G:/My Drive/Documents/research/giraffe")
-setwd("L:/projects/etosha_heights/")
-=======
-######################################
->>>>>>> cca79b7521834a3f0a8dcfeb132a98e9fe6d3eb2
+setwd("G:/My Drive/Documents/research/giraffe")
+#setwd("L:/projects/etosha_heights/")
+
+# list data files
+df <- list.files(path = "SLEEC_met_data/", pattern = ".csv", full.names = T)
+
+
+# test some code
+t <- read.csv(df[1], skip = 2, header = T)
