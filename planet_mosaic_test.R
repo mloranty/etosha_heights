686e6cbe60f8735d5317725641f174c47c223eec
Mike Loranty <mloranty@colgate.edu>
2024-11-08 10:55:37 -0500

    adding scripts to test mosaicing options for planet data in order to deal with seams between scenes

--- planet_mosaic_test.R
+++ planet_mosaic_test.R
@@ -20,7 +20,24 @@ library(randomForest)
 
 #set working directory 
 setwd("L:/projects/etosha_heights/eh_planet_mosaic_tests/")
+# define evi function for 8-band planet images
+# note the reflectance values are divided by 10000
+evi <- function(x){
+  b <- x[[2]]/10000
+  r <- x[[6]]/10000
+  n <- x[[8]]/10000
+  2.5*((n-r)/(n+6*r-7.5*b+1))
+}
 
+# define near infrared reflectance of vegetation for 8-band planet images
+#  Badgley et al 2017
+# note the reflectance values are divided by 10000
+
+ndvi <- function(x){
+  r <- x[[6]]/10000
+  n <- x[[8]]/10000
+  ((n-r)/(n+r))
+}
 # get list of files
 f <- list.files(path = "EH_20240519-nh_psscene_analytic_8b_sr_udm2/",
                 patter = glob2rx("*Analytic*.tif"), 
@@ -45,8 +62,9 @@ ehm <- mosaic(ehi, fun = "mean",
               filename = "EH_20240519_mosaic_test.tif")
 
 
+ehn <- ndvi(ehm)
+writeRaster(ehn, filename = "EH_20240519_mosaic_test_ndvi.tif" )
 
-
-
-
+ehe <- evi(ehm)
+writeRaster(ehe, filename = "EH_20240519_mosaic_test_evi.tif" )
 
